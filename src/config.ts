0135de3598adc82702550e8079eae1a73f7b1e53
MajorDionysus <161795560+MajorDionysus@users.noreply.github.com>
2025-04-12 20:38:39 +0800

    20250412

--- src/config.ts
+++ src/config.ts
@@ -12,12 +12,12 @@ export const siteConfig: SiteConfig = {
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
     src: "https://img.mrj.today/images/cover/banner.jpg",
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    position: 'top',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
       text: '',              // Credit text to be displayed
